e6669c1224af7ea001bb80165a235dd4559d03ee
Jonathan Chan <jhchan0321@gmail.com>
2016-01-16 11:06:09 -0800

    jury model

---
+++ JMS/Models/JuryModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JMS.Models
+{
+    public class JuryModel
+    {
+        public int JuryId { get; set; }
+        public bool CheckedIn { get; set; }
+        public bool Completed { get; set; }
+        public int DaysAvailable { get; set; }
+    }
+}
