54b9721b3305ab50481c3d3ba84ebdc43a724d2e
Jonathan Chan <jhchan0321@gmail.com>
2016-01-15 22:43:04 -0800

    initial push

---
+++ JMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JMS.Startup))]
+namespace JMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
