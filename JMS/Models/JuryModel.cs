5a53ada670d924e1fe843157a83a4c1f2a6ec6f7
Jonathan Chan <jhchan0321@gmail.com>
2016-01-16 13:37:11 -0800

    initial static list plus check in and completed functions

--- JMS/Models/JuryModel.cs
+++ JMS/Models/JuryModel.cs
@@ -7,7 +7,7 @@ namespace JMS.Models
 {
     public class JuryModel
     {
-        public int JuryId { get; set; }
+        public Int32 JuryId { get; set; }
         public bool CheckedIn { get; set; }
         public bool Completed { get; set; }
         public int DaysAvailable { get; set; }
