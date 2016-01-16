8d48d02fbccde98dcc335468d1d2b4f9b661a9ef
FullStackPatel <patel.brijesh821.gmail.com>
2016-01-16 14:27:52 -0800

    SendGrid API
    Test Email was success need to edit template html

--- JMS/Controllers/MessageApiController.cs
+++ JMS/Controllers/MessageApiController.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using JMS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using JMS.Services;
 
 namespace JMS.Controllers
 {
@@ -11,5 +13,20 @@ namespace JMS.Controllers
     [RoutePrefix("api/message")]
     public class MessageApiController : ApiController
     {
+        
+        [Route("ConfirmEmail"), HttpPost]
+        public async System.Threading.Tasks.Task<HttpResponseMessage> SendConfirmMail(MailModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+           
+           await  MessagingService.SendConfirmationEmail(model);
+
+            return Request.CreateResponse();
+
+        }
+
     }
 }
---
+++ JMS/Models/AttendeesEmail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JMS.Models
+{
+    public class AttendeesEmail : JuryModel
+    {
+        [Required]
+        public string Email { get; set; }
+    }
+}
