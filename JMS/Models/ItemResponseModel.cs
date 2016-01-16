04e527cf3712763325b1d51f4a466ad6f8e247d3
Jonathan Chan <jhchan0321@gmail.com>
2016-01-16 11:42:32 -0800

    item response model

---
+++ JMS/Models/ItemResponseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JMS.Models
+{
+    public class ItemResponseModel
+    {
+
+        public class ItemResponse<T>
+        {
+
+            public T Item { get; set; }
+
+        }
+    }
+}
